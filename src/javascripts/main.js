7c56da54d331a7571c9bba7116dd9c8ca570bd6d
Ryan McNair <ryan.mcnair@gmail.com>
2020-09-23 15:42:41 -0500

    added projects to firebase and got the shit to work right

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
-import projectcards from './components/projectcards';
+// import projectcards from './components/projectcards';
 import build from './components/projectCardsFirebase';
 
 import '../styles/main.scss';
@@ -8,7 +8,7 @@ import '../styles/main.scss';
 const init = () => {
   build.buildProjectCards();
   firebase.initializeApp(apiKeys.firebaseKeys);
-  projectcards.createProjectCard();
+  // projectcards.createProjectCard();
 };
 
 init();
